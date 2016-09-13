6a6599ee4a175ebdfde6527afcd219c54c40d2dd
StlMaris123 <stellamarisnjage@gmail.com>
2016-09-13 13:15:44 +0300

    golf game

---
+++ html_files/js/golf.js
@@ -0,0 +1,21 @@
+function golfScore(par, strokes) {
+    if (strokes === 1){
+        console.log( "Hole-in-one!");
+    }
+    else if(strokes <= par - 2){
+        console.log( "Eagle");
+    }else if (strokes == par - 1){
+        console.log( "Birdie");
+    } else if (strokes == par){
+        console.log( "Par");
+    }else if(strokes == par +  1){
+        console.log( "Bogey");
+    } else if(strokes == par + 2){
+        console.log("Double Bogey");
+    }else if (strokes >= par +3){
+        console.log("Go Home!");
+    }
+}
+
+golfScore(3, 3);
+
