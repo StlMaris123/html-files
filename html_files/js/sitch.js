51d7eb121f4f5924b8a06b52efe0dbfcee34c178
StlMaris123 <stellamarisnjage@gmail.com>
2016-09-13 20:41:30 +0300

    switch statement

---
+++ html_files/js/sitch.js
@@ -0,0 +1,18 @@
+function testSwitch(val){
+    switch(val){
+        case 1:
+            console.log("best grade");
+            break;
+        case 2:
+            console.log("good grade");
+            break;
+        case 3:
+            console.log("average grade");
+            break;
+        case 4:
+            console.log("poor grade");
+            break;
+    }
+
+}
+testSwitch(4);
