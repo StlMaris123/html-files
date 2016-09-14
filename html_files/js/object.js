e02717c3bec7ba5120d274ea307a3d889bd835d3
StlMaris123 <stellamarisnjage@gmail.com>
2016-09-14 12:30:25 +0300

    use the dot operator to read an object property

---
+++ html_files/js/object.js
@@ -0,0 +1,8 @@
+ var mySelf = {
+    "name" : "Stella",
+    "age" : 30,
+    "shirt" : "whhite"
+};
+//accessiing elements using the dot operator
+var myName = mySelf.name;
+var myAge = mySelf.age;
