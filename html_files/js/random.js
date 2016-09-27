ac83118e68ddc696171117945347f4ea55425cc5
StlMaris123 <stellamarisnjage@gmail.com>
2016-09-27 08:54:56 +0300

    return a random number between 0 and 19

--- html_files/js/random.js
+++ html_files/js/random.js
@@ -1,3 +1,8 @@
+// generates random decimal number
 function randomNumber(){
    return Math.random();
+}
+// Generate random whole numbers
+function wholeNumber(){
+    return Math.floor(Math.random() * 20);
 }
